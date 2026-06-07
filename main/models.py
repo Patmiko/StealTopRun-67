b3f8f0f80d76d6cc1b909928b1589bf81f6b0bce
patmik <patmikdev@gmail.com>
2026-06-07 22:28:56 +0200

    feat: add new main app and base model ofr category and gAME

---
+++ main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    release_date = models.DateField()
+    img_url = models.URLField(max_length=500, blank=True)
+
+    categories = models.ManyToManyField(Category, related_name='games')
+
+    def __str__(self):
+        return self.name
