e46b41928c467477cee4eaab3927e7836a6a12d2
patmik <patmikdev@gmail.com>
2026-06-07 23:10:32 +0200

    feat: add speedruns model and modify other to fit uml better

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Game, Category, SpeedrunType
+from .models import Game, Category, Speedrun, SpeedrunType
 
 
 @admin.register(Category)
@@ -11,11 +11,18 @@ class CategoryAdmin(admin.ModelAdmin):
 class GameAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'release_date')
     search_fields = ('name',)
-    list_filter = ('release_date', 'categories')
-    filter_horizontal = ('categories',)
+    list_filter = ('release_date',)
+
 
 @admin.register(SpeedrunType)
 class SpeedrunTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'game')
     search_fields = ('name', 'game__name')
     list_filter = ('game',)
+
+
+@admin.register(Speedrun)
+class SpeedrunAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'time', 'date', 'status', 'speedtrun_type', 'user')
+    search_fields = ('url', 'speedtrun_type__name', 'user__username')
+    list_filter = ('status', 'date')
--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='categories', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -16,8 +17,6 @@ class Game(models.Model):
     release_date = models.DateField()
     img_url = models.URLField(max_length=500, blank=True)
 
-    categories = models.ManyToManyField(Category, related_name='games')
-
     def __str__(self):
         return self.name
 
@@ -28,3 +27,22 @@ class SpeedrunType(models.Model):
 
     def __str__(self):
         return f"{self.game.name} - {self.name}"
+
+
+class Speedrun(models.Model):
+    url = models.CharField(max_length=255)
+    time = models.FloatField()
+    date = models.DateField()
+
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        REJECTED = 'REJECTED', 'Rejected'
+        ACCEPTED = 'ACCEPTED', 'Accepted'
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+    speedtrun_type = models.ForeignKey(SpeedrunType, on_delete=models.CASCADE, related_name='speedruns')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='speedruns')
