5b4b44a9a0292c465dc2755c413727ff46a2f276
patmik <patmikdev@gmail.com>
2026-06-15 21:27:07 +0200

    feat: add filtering for speedrun api view

--- api/views.py
+++ api/views.py
@@ -2,20 +2,26 @@ from rest_framework import viewsets
 from main.models import Game, Speedrun, Status
 from .serializers import PublicGameSerializer, PublicSpeedrunSerializer
 
+
 class PublicGameViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    Public endpoint to view all registered games and their categories.
-    """
     queryset = Game.objects.prefetch_related('categories').all()
     serializer_class = PublicGameSerializer
 
+
 class PublicSpeedrunViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    Public endpoint to view ACCEPTED speedruns.
-    """
     serializer_class = PublicSpeedrunSerializer
 
     def get_queryset(self):
-        return Speedrun.objects.filter(
+        queryset = Speedrun.objects.filter(
             status=Status.ACCEPTED
         ).select_related('user', 'speedrun_type__game')
+
+        game_id = self.request.query_params.get('game')
+        if game_id:
+            queryset = queryset.filter(speedrun_type__game_id=game_id)
+
+        category_id = self.request.query_params.get('category')
+        if category_id:
+            queryset = queryset.filter(speedrun_type_id=category_id)
+
+        return queryset
